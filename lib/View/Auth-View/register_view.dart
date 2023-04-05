ecc35ac7912056abcb5e76f68d6414031640ed48
Mehroz <mehrozhassan99@gmail.com>
2023-04-05 20:49:21 +0500

    inital

---
+++ lib/View/Auth-View/register_view.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class RegisterView extends StatelessWidget {
+  const RegisterView({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return SafeArea(
+        child: Scaffold(
+      appBar: AppBar(
+        title: const Text("Register"),
+      ),
+    ));
+  }
+}
